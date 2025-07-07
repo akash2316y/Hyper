ae25ad214445398c6e3ba542fd37b9c532bbe0f1
akash2316y <iamakashrabha@gmail.com>
2025-07-07 13:45:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,23 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            "techvj login",
+            "techvj login",    # Session name ➔ will generate .session file
             api_id=API_ID,
             api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
             plugins=dict(root="plugins"),
             workers=50,
             sleep_threshold=10
         )
 
-      
     async def start(self):
-            
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Userbot Started')
 
     async def stop(self, *args):
-
         await super().stop()
-        print('Bot Stopped Bye')
+        print('Userbot Stopped')
 
 Bot().run()
