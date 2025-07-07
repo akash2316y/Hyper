81e9acb708f27d637457136216647623ec2a9363
akash2316y <iamakashrabha@gmail.com>
2025-07-07 14:05:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,36 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from pyrogram import Client
-from config import API_ID, API_HASH
+from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            "techvj login",    # Session name ➔ will generate .session file
+            "techvj login",
             api_id=API_ID,
             api_hash=API_HASH,
-            plugins=dict(root="plugins"),
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="TechVJ"),
             workers=50,
             sleep_threshold=10
         )
 
+      
     async def start(self):
+            
         await super().start()
-        print('Userbot Started')
+        print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
+
         await super().stop()
-        print('Userbot Stopped')
+        print('Bot Stopped Bye')
 
 Bot().run()
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
