240b5d3f0f18a4031725dbee500159217e884b86
akash2316y <iamakashrabha@gmail.com>
2025-07-07 15:24:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -10,9 +10,10 @@ class Database:
 
     def new_user(self, id, name):
         return dict(
-            id = id,
-            name = name,
-            session = None,
+            id=id,
+            name=name,
+            session=None,
+            channel_id=None,   # ✅ Added channel_id field for future use
         )
     
     async def add_user(self, id, name):
@@ -20,7 +21,7 @@ class Database:
         await self.col.insert_one(user)
     
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return bool(user)
     
     async def total_users_count(self):
@@ -38,6 +39,19 @@ class Database:
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('session')
+        if user:
+            return user.get('session')
+        return None
 
+    async def set_channel(self, id, channel_id):
+        await self.col.update_one({'id': int(id)}, {'$set': {'channel_id': channel_id}})
+
+    async def get_channel(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        if user:
+            return user.get('channel_id')
+        return None
+
+
+# Initialize the database object
 db = Database(DB_URI, DB_NAME)
